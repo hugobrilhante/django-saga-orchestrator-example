281499c71e9d718e47bf8ad3875f3b7f8999d997
Hugo Brilhante <hugobrilhante@gmail.com>
2024-02-08 16:54:42 -0300

    feat: Add orchestrator

---
+++ order/src/core/interfaces.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+
+class ActionAbstract(ABC):
+    @abstractmethod
+    def perform(self, data: dict, sender: str, transaction_id: str) -> tuple:
+        pass
+
+
+class CompensationAbstract(ABC):
+    @abstractmethod
+    def compensate(self, data: dict, sender: str, transaction_id: str) -> tuple:
+        pass
+
+
+class PublisherAbstract(ABC):
+    @abstractmethod
+    def send(self, destination: str, body: dict) -> None:
+        pass
